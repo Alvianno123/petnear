1f506685ac5e45a870750e517293f0a966bfe14e
Alvianno Wijaya <alviannowijaya@gmail.com>
2023-09-07 13:19:42 +0700

    Icon Launcer

--- lib/Api/apiConnect.dart
+++ lib/Api/apiConnect.dart
@@ -1,5 +1,5 @@
 class API {
-  static const HOST_PATH = "http://192.168.1.9:8080/petnear";
+  static const HOST_PATH = "http://192.168.80.150:8080/petnear";
 
   static const USER_ENDPOINT = "$HOST_PATH/login";
 
