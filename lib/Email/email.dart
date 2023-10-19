2f41af5bf0f11788fb9d936e4103c40e82ce3f46
Alvianno Wijaya <alviannowijaya@gmail.com>
2023-10-19 13:55:23 +0700

    email

--- lib/Email/email.dart
+++ lib/Email/email.dart
@@ -17,12 +17,16 @@ sendEmail(BuildContext context //For showing snackbar
 
   // Create our message.
   final message = Message()
-        ..from = Address(username, 'Ahmed Usman')
+        ..from = Address(username, 'PetNear')
         ..recipients.add('alviannowijaya@gmail.com')
-        // ..ccRecipients.addAll(['abc@gmail.com', 'xyz@gmail.com']) // For Adding Multiple Recipients
+        ..ccRecipients.addAll([
+          'richabellark@gmail.com',
+          'xyz@gmail.com'
+        ]) // For Adding Multiple Recipients
         // ..bccRecipients.add(Address('a@gmail.com')) For Binding Carbon Copy of Sent Email
-        ..subject = 'Mail from Mailer'
-        ..text = 'Hello dear, I am sending you email from Flutter application'
+        ..subject = 'PetNear - Forgot Password'
+        ..text =
+            'Hello dear, I am sending you email from Flutter application \n PETNEAR'
       // ..html = "<h1>Test</h1>\n<p>Hey! Here's some HTML content</p>"; // For Adding Html in email
       // ..attachments = [
       //   FileAttachment(File('image.png'))  //For Adding Attachments
