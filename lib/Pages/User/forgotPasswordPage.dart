7a5ec265e4c631236594a31d7bb99359be644dd7
Alvianno Wijaya <alviannowijaya@gmail.com>
2023-10-19 13:46:07 +0700

    Email

--- lib/Pages/User/forgotPasswordPage.dart
+++ lib/Pages/User/forgotPasswordPage.dart
@@ -1,8 +1,15 @@
+import 'dart:convert';
+import 'package:mailer/mailer.dart';
+import 'package:petnear/Email/email.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:petnear/Api/apiConnect.dart';
 
 class ForgotPasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -22,17 +29,63 @@ class ForgotPasswordPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Stack(
+
+      ////////////////////////BODY////////////////////
+      body: Form(
+        key: formKey,
+        child: ListView(
           children: <Widget>[
-            Align(
-              alignment: Alignment.center,
-              child: Text('Halo Forgot', style: TextStyle(fontSize: 25.0)),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Text('SILAHKAN MASUKAN E-MAIL ANDA !!!'),
             ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: TextFormField(
+                validator: (value) {
+                  if (value == null || value == '') {
+                    return 'Enter your E-mail.';
+                  }
+                  return null;
+                },
+                //   controller: user,
+                decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 1, horizontal: 10),
+                  border: OutlineInputBorder(),
+                  labelText: 'E-Mail',
+                  hintText: 'E-Mail',
+                ),
+                style: TextStyle(fontSize: 16),
+                maxLines: 1, // <-- SEE HERE
+                minLines: 1, // <-- SEE HERE
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      sendEmail(context);
+                      //   tampil();
+                    }
+                  },
+                  child: const Text('Registrasi / Daftar'),
+                )),
           ],
         ),
       ),
     );
   }
+}
+
+void tampil() {
+  Fluttertoast.showToast(
+      msg: "Forgot Password Sudah Berhasil, Silahkan Login !!!",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.green,
+      textColor: Colors.white,
+      fontSize: 16.0);
 }
